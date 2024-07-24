9772f09b1ec871b020190f4536561422cceb089e
chunlee-thong <chunlee.thong@gmail.com>
2024-07-24 14:04:51 +0700

    Update to run on web

--- lib/src/home_page.dart
+++ lib/src/home_page.dart
@@ -1,10 +1,28 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:googleapis_auth/auth_io.dart';
 import 'package:http/http.dart' as http;
+
+Future<Map<String, dynamic>> obtainCredentials(PlatformFile serviceAccountFile) async {
+  var accountCredentials = ServiceAccountCredentials.fromJson(jsonDecode(
+    await serviceAccountFile.xFile.readAsString(),
+  ));
+  List<String> scopes = ["https://www.googleapis.com/auth/firebase.messaging"];
+
+  var client = http.Client();
+  AccessCredentials accessCredential = await obtainAccessCredentialsViaServiceAccount(
+    accountCredentials,
+    scopes,
+    client,
+  );
+
+  client.close();
+  return accessCredential.toJson();
+}
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,33 +33,23 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   TextEditingController textEditingController = TextEditingController();
-  File? serviceAccountFile;
+  PlatformFile? serviceAccountFile;
   Map<String, dynamic>? credential;
 
   Future<void> pickFile() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles();
     if (result != null) {
-      serviceAccountFile = File(result.files.single.path!);
-      textEditingController.text = serviceAccountFile!.path;
+      serviceAccountFile = result.files.first;
+      textEditingController.text = serviceAccountFile!.xFile.path;
       setState(() {});
     }
   }
 
-  Future<void> obtainCredentials() async {
-    var accountCredentials = ServiceAccountCredentials.fromJson(jsonDecode(serviceAccountFile!.readAsStringSync()));
-    List<String> scopes = ["https://www.googleapis.com/auth/firebase.messaging"];
-
-    var client = http.Client();
-    AccessCredentials accessCredential = await obtainAccessCredentialsViaServiceAccount(
-      accountCredentials,
-      scopes,
-      client,
-    );
-
+  Future generateCredential() async {
+    final result = await compute(obtainCredentials, serviceAccountFile!);
     setState(() {
-      credential = accessCredential.toJson();
+      credential = result;
     });
-    client.close();
   }
 
   @override
@@ -73,15 +81,18 @@ class _HomePageState extends State<HomePage> {
             ),
             const SizedBox(height: 16),
             ElevatedButton(
-              onPressed: serviceAccountFile != null ? obtainCredentials : null,
+              onPressed: serviceAccountFile != null ? generateCredential : null,
               child: const Text("Generate"),
             ),
             if (credential != null) ...[
               const SizedBox(height: 24),
               SelectableText(getPrettyJSONString(credential)),
               ElevatedButton(
-                onPressed: () {},
-                child: const Text("Copy"),
+                onPressed: () {
+                  var token = credential?["accessToken"]?["data"] ?? "";
+                  Clipboard.setData(ClipboardData(text: token));
+                },
+                child: const Text("Copy Token"),
               ),
             ],
           ],
